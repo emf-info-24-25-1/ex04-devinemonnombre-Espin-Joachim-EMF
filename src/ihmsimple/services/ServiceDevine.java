cf5fac3631350ce7088da77b6704b20f01f9a7f9
Sedryx_ <joachim.espin76@gmail.com>
2025-03-17 20:34:34 +0100

    fini

--- src/ihmsimple/services/ServiceDevine.java
+++ src/ihmsimple/services/ServiceDevine.java
@@ -1,5 +1,7 @@
 package ihmsimple.services;
 
+import ihmsimple.ctrl.Controller;
+
 /**
  * Classe représentant le service de l'application MVC "IhmSimple".
  * 
@@ -11,24 +13,24 @@ public class ServiceDevine {
     /**
      * Nombre maximum que l'utilisateur peut proposer.
      */
-     // VOTRE CODE ICI...
+     public static final int NOMBRE_MAX = 100;
     
     /**
      * Nombre minimum que l'utilisateur peut proposer.
      */
-     // VOTRE CODE ICI...
+     public static final int NOMBRE_MIN = 1;
     
     /**
      * Valeur retournée par la méthode lireValeurProposee() si ce qui a été saisi
      * par l'utilisateur n'est pas convertible
      * en un entier.
      */
-     // VOTRE CODE ICI...
+     public static final int NOMBRE_INVALIDE = -1;
 
     /**
      * Référence au contrôleur de l'application.
      */
-    // VOTRE CODE ICI...
+    private Controller refCtrl;
 
     /**
      * Constructeur du service.
@@ -37,26 +39,27 @@ public class ServiceDevine {
      * initiale donner.
      */
     public ServiceDevine() {
-        // VOTRE CODE ICI...
+        this.refCtrl = null;
     }
 
     /**
      * Invente et retourne un nouveau nombre à deviner aléatoirement choisi entre
      * les limites tolérées pour ce jeu.
-     * 
+     *
      * @return le nombre à deviner aléatoirement choisi
      */
     public int penserAUnNombre() {
-        // VOTRE CODE ICI...
-    }
+        int nombreAleatoir = (int) (Math.random()*(NOMBRE_MIN + NOMBRE_MAX)) - NOMBRE_MIN;
+        return nombreAleatoir;
+    };
 
     /**
      * Retourne la référence au contrôleur de l'application.
-     * 
+     *
      * @param refCtrl la référence au contrôleur de l'application
      */
     public void setRefCtrl(Controller refCtrl) {
-        // VOTRE CODE ICI...
+        this.refCtrl = refCtrl;
     }
 
     /**
@@ -65,7 +68,7 @@ public class ServiceDevine {
      * @return la référence au contrôleur de l'application
      */
     public Controller getRefCtrl() {
-        // VOTRE CODE ICI...
+        return refCtrl;
     }
 
 }
